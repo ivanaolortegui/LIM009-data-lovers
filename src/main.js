70148857f8a1cb29ea16591358c015a3ea707283
ivanaolortegui <olorteguimor@gmail.com>
2019-03-14 17:55:00 -0500

    Recorriendo la data y pintadola los nombres en el html

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
 /* Manejo del DOM */
+const root = document.getElementById('root');
+
+const valuesOfData = Object.values(LOL.data);
+let dataName = '';
+valuesOfData.forEach(ele => {
+dataName +=ele.id;
+const listData =  ` <h2> ${dataName} </h2>
+<img src="${ele.img}"/>
+`
+root.innerHTML = listData;
+});
