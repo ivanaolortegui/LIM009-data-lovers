b83ab3eeb25b4f24c5747bb7cb62b4d97712eb8e
ivanaolortegui <olorteguimor@gmail.com>
2019-03-16 15:05:02 -0500

    Mostrando el splash de los campeones y ocultando la imagen chica de ellos con evento click

--- src/main.js
+++ src/main.js
@@ -1,17 +1,39 @@
 /* Manejo del DOM */
 const root = document.getElementById('root');
+
+/* data*/
 const valuesOfData = Object.values(LOL.data);
+
 const dataTemplate = data => {
-  let dataToHtml = '';
-  data.forEach(ele => {
+  let dataToHtml = [];
+  data.forEach((ele, index) => {
     const listData = `
-      <h2> ${ele.id} </h2>
-      <img id="img-campions" src="${ele.img}"/>
-      <h2> ${ele.tags.join(' ')} </h2>
-      `
-    dataToHtml += listData;
+     <div id=${index} class="show">
+      <div>
+        <h2> ${ele.id} </h2>
+        <img id=${index}-${ele.id} src="${ele.img}"/>
+        
+      </div>
+      <div id=${index}-${ele.id} class="hidden">
+         <img src="${ele.splash}"/>
+         <h3 > ${ele.title} </h3>
+         <h2> ${ele.tags.join(' ')} </h2>
+         <h3> ${ele.stats.attackdamage}</h3>
+      </div>
+      </div>`
+    dataToHtml.push(listData);
   });
-  root.innerHTML = dataToHtml;
+  root.innerHTML = dataToHtml.join(' ');
 };
 dataTemplate(valuesOfData);
+
+root.addEventListener('click', (e) => {
+  const string = e.target.id; // accede al id de la etiqueta que se hace click
+  const index = string.substr(0, string.indexOf('-'));
+  e.currentTarget.children[index].lastElementChild.setAttribute("class", "show");
+  const targetOfRoot = e.currentTarget.children;
+  for (let i = 0; i < targetOfRoot.length; i++) {
+    e.currentTarget.children[i].firstElementChild.setAttribute("class", "hidden")
+  }
+})
 
