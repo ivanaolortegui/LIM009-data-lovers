e83186e474ffc5d5231034227ded9bbef78e0c57
ivanaolortegui <olorteguimor@gmail.com>
2019-03-23 13:59:05 -0500

    Mostrando de forma dinamica los iconos de los roles de los campeones

--- src/main.js
+++ src/main.js
@@ -3,38 +3,38 @@ const root = document.getElementById('root');
 const select = document.getElementById('selectRole');
 const radio = document.getElementById('radio');
 const botonTop = document.getElementById('backTop');
-/* const asc = document.getElementById('r1');
-const desc = document.getElementById('r2');
-const majorDamage = document.getElementById('r3');
-const minorDamage = document.getElementById('r4'); */
 
 /* data*/
 const valuesOfData = Object.values(LOL.data);
 const dataTemplate = data => {
   let dataToHtml = [];
   data.forEach((ele, index) => {
-    const listData = `
-      <div id=${index} class="show">
-        <div class="col-xs-4 col-sm-13 col-md-2">
-          <h2 class="col-xs-12 col-sm-12 col-md-12"> ${ele.id} </h2>
-          <img class="col-xs-12 col-sm-12 col-md-12" id=${index}-${ele.id} src="${ele.splash}" alt="imagen de ${ele.id}"/>  
-        </div>
-        <div id=${index}-${ele.id} class="hidden">
-          <figure class= "col-md-12">
-            <img class= "col-xs-8 col-sm-3 col-md-8"src="${ele.splash}"/>
-            <figcaption>
-              <h3> ${ele.title} </h3>
-              <img class="icon-img" src="img/asesino-shop.png" <h2> ${ele.tags.join(' ')} </h2>
-              <h3> ${ele.stats.attackdamage}</h3>     
-            </figcaption>
-          </figure>
-        </div>
-      </div>`;
+    let listData = `
+     <div id=${index} class="show">
+       <div class="col-xs-4 col-sm-13 col-md-2">
+         <h2 class="col-xs-12 col-sm-12 col-md-12"> ${ele.id} </h2>
+         <img class="col-xs-12 col-sm-12 col-md-12" id=${index}-${ele.id} src="${ele.splash}" alt="imagen de ${ele.id}"/> 
+       </div>
+       <div id=${index}-${ele.id} class="hidden">
+         <figure class= "col-md-12">
+           <img class= "col-xs-8 col-sm-3 col-md-8"src="${ele.splash}"/>
+           <figcaption>
+             <h3> ${ele.title} </h3>`;
+    ele.tags.forEach((tag) => {
+      listData += `<img class="icon-img" src="img/${tag}.png">${tag}`;
+    });
+
+    listData += `<h3> ${ele.stats.attackdamage}</h3>    
+           </figcaption>
+         </figure>
+       </div>
+     </div>`;
     dataToHtml.push(listData);
   });
   root.innerHTML = dataToHtml.join(' ');
 };
 dataTemplate(valuesOfData);
+
 
 root.addEventListener('click', (event) => {
   const string = event.target.id; // accede al id de la etiqueta que se hace click
@@ -61,7 +61,7 @@ botonTop.addEventListener('click', () => {
   document.documentElement.scrollTop = 0;
 });
 
-window.onscroll = function() {
+window.onscroll = function () {
   scrollFunction();
 };
 
