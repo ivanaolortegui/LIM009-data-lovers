cb1d863a2ac49b442e87b217c0a97423c96276af
ivanaolortegui <olorteguimor@gmail.com>
2019-04-04 21:34:07 -0500

    Agregando comentarios a data.js

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 /* Manejo de data */
-
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 window.data = {
+  // pushando a cada elemento (tags) del array que es valor de la propiedad tags del array de la data
   getAllChampsTags: (dataCampeones) => {
     const arr = [];
     dataCampeones.forEach(tag => tag.tags.map(el => arr.push(el)));
@@ -10,6 +10,7 @@ window.data = {
   },
   // funcion refactorizada usando condicionales ternarios 
   getAllUniqueChampsTags: (arrTags) => {
+    // aqui filtro los tags que no se entran aun en el array, comprando y buscandolo con un indexOf y que me lo retorne si no lo encuentra 
     const uniqueTags = [];
     arrTags.forEach((elem) => uniqueTags.indexOf(elem) === -1 ? uniqueTags.push(elem) : uniqueTags);
     return uniqueTags;
@@ -40,20 +41,15 @@ window.data = {
   computeStats: (data, tags, feature) => {
     // 1 parameter: data
     // Statistics
+    // primero filtro todos los tags; y luego mapeo e ingreso al objeto info y obtengo a su valor que puede ser attack o dificulty
     const tagsArr = data.filter(value => value.tags.includes(tags));
     const sumTagsElements = tagsArr.map((arr) => arr.info[feature]);
     // sumTagsElements.sort((elementA, elementB) => elementA - elementB); ese era el error codigo muerto 
-    let sum = 0;
+    let accumulator = 0;
     sumTagsElements.forEach((ele) => {
-      sum += ele;
+      accumulator += ele;
     });
-    const featureTagStats = (sum / sumTagsElements.length).toFixed(2);
-
-    /*  console.log(tagsArr);
-    const arrEle = [];
-    const totalArr = [tagsArr, arrMelee, arrAssassin].forEach(ele => ele.map((arr) => arrEle.push(arr.info.attack)));
-    console.log(arrEle);
-     */
+    const featureTagStats = (accumulator / sumTagsElements.length).toFixed(2);
     return featureTagStats;
   }
 };
